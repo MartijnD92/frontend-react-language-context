2660075f87faa3c4f2e7d0c1020737532bf54f33
Martijn Doensen <martijn.doensen@live.nl>
2021-04-08 20:19:43 +0200

    Finish assignment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import AboutUs from './pages/aboutUs/AboutUs';
 import AllPlants from './pages/allPlants/AllPlants';
 
 function App() {
-  return (
-    <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about-us">
-            <AboutUs />
-          </Route>
-          <Route path="/all-plants">
-            <AllPlants />
-          </Route>
-        </Switch>
-    </Router>
-  );
+	return (
+		<>
+			<Header />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/about-us">
+					<AboutUs />
+				</Route>
+				<Route path="/all-plants">
+					<AllPlants />
+				</Route>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
